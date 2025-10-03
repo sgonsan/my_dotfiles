9421bd999f3dd47358b49ae5fea037f656e2a7f3
alu0101513379 <alu0101513379@ull.edu.es>
2025-10-03 21:20:37 +0100

    .

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,12 +4,13 @@ if status is-interactive
     starship init fish | source
 end
 
+eval "$(vivid generate dracula | sed 's/^/export LS_COLORS="/;s/$/"/')"
+
 # List Directory
-alias l='eza -lh  --icons=auto' # long list
-alias ls='eza -1   --icons=auto' # short list
-alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
-alias ld='eza -lhD --icons=auto' # long list dirs
-alias lt='eza --icons=auto --tree' # list folder as tree
+# alias l='eza -lh --icons=auto --sort=name --group-directories-first' # long list
+# alias ls='eza --icons=auto' # short list
+# alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
+# alias lt='eza -lhaT --icons=auto' # list folder as tree
 
 # Handy change dir shortcuts
 abbr .. 'cd ..'
@@ -20,3 +21,11 @@ abbr .5 'cd ../../../../..'
 
 # Always mkdir a path (this doesn't inhibit functionality to make a single dir)
 abbr mkdir 'mkdir -p'
+
+alias pacup='sudo pacman -Syyu'
+alias yayup='yay -Syyu'
+alias yayrm='yay -Yc'
+alias n='nvim'
+alias fast='fastfetch'
+
+thefuck --alias | source
