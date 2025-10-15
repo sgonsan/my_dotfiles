a39ce046e1e0efe34c754812ddf6d3ceb62601d3
alu0101513379 <alu0101513379@ull.edu.es>
2025-10-15 01:16:23 +0100

    merge

--- fish/.config/fish/conf.d/editor.fish
+++
@@ -1,4 +0,0 @@
-# Editor por defecto – se ejecuta en cada nueva shell
-set -gx EDITOR nvim
-set -gx VISUAL nvim
-
--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,31 +1,68 @@
-set -g fish_greeting
+# ===================================================================
+# 🐟 FISH CONFIG
+# ===================================================================
+
+# -------------------- Basic setup --------------------
+set -g fish_greeting    # Disable default greeting
 
 if status is-interactive
     starship init fish | source
 end
 
-eval "$(vivid generate dracula | sed 's/^/export LS_COLORS="/;s/$/"/')"
+# Default editor
+set -gx EDITOR nvim
+set -gx VISUAL nvim
 
-# List Directory
-# alias l='eza -lh --icons=auto --sort=name --group-directories-first' # long list
-# alias ls='eza --icons=auto' # short list
-# alias ll='eza -lha --icons=auto --sort=name --group-directories-first' # long list all
-# alias lt='eza -lhaT --icons=auto' # list folder as tree
+# -------------------- Modern CLI tools --------------------
+# BAT as CAT (Dracula theme)
+if type -q bat
+    alias cat="bat --paging=never --theme=Dracula"
+end
 
-# Handy change dir shortcuts
+# EZA instead of ls
+if type -q eza
+    if type -q vivid
+        set -gx LS_COLORS (vivid generate dracula)
+    end
+    alias l='eza -lh --icons=auto --sort=name --group-directories-first --git'
+    alias ls='eza --icons=auto --git'
+    alias ll='eza -lah --header --icons=auto --sort=name --group-directories-first --git'
+    alias lt='eza -lhaT --icons=auto'
+end
+
+# Zoxide as cd replacement
+if type -q zoxide
+    zoxide init fish | source
+    alias c='z'
+end
+
+# Dracula theme for fish syntax
+fish_config theme choose "Dracula"
+
+# -------------------- Handy aliases --------------------
+alias pacup='sudo pacman -Syyu'
+alias yayup='yay -Syyu --noconfirm'
+alias yayrm='yay -Scc --noconfirm ; yay -Yc --noconfirm'
+alias n='nvim'
+alias fast='fastfetch'
+alias diff='difft'
+thefuck --alias | source
+
+# -------------------- Directory abbreviations --------------------
 abbr .. 'cd ..'
 abbr ... 'cd ../..'
 abbr .3 'cd ../../..'
 abbr .4 'cd ../../../..'
 abbr .5 'cd ../../../../..'
-
-# Always mkdir a path (this doesn't inhibit functionality to make a single dir)
 abbr mkdir 'mkdir -p'
 
-alias pacup='sudo pacman -Syyu'
-alias yayup='yay -Syyu'
-alias yayrm='yay -Yc'
-alias n='nvim'
-alias fast='fastfetch'
+# -------------------- Yazi wrapper --------------------
+function y
+    set tmp (mktemp -t "yazi-cwd.XXXXXX")
+    yazi $argv --cwd-file="$tmp"
+    if set cwd (command cat -- "$tmp"); and [ -n "$cwd" ]; and [ "$cwd" != "$PWD" ]
+        builtin cd -- "$cwd"
+    end
+    rm -f -- "$tmp"
+end
 
-thefuck --alias | source
